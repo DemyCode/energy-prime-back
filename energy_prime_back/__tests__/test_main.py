8ae40774be7b61985b615a4af63ef846c1cef71d
DemyCode <verycols@gmail.com>
2023-03-11 11:17:37 +0100

    fix: removing ping test

--- energy_prime_back/__tests__/test_main.py
+++ energy_prime_back/__tests__/test_main.py
@@ -11,12 +11,6 @@ def test_root():
     assert response.json() == {"msg": "Hello World"}
 
 
-def test_ping():
-    response = client.get("/ping")
-    assert response.status_code == 200
-    assert response.json() == {"ping": "pong"}
-
-
 def test_get_user():
     response = client.get("/users")
     assert response.status_code == 200
